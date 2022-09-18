32127ad792bfcfbdc77842908dfa52d459ed7fe1
BolotnikovMS <bolotnikovms@yandex.ru>
2022-09-18 17:42:11 +0500

    added interactive

--- js/main.js
+++ js/main.js
@@ -24,11 +24,55 @@ document.addEventListener('DOMContentLoaded', () => {
 		// }
 	})
 
+//	Link scroll
+	const basket = document.querySelector('.basket')
+
+	function linkScroll(nav) {
+		const links = nav.querySelectorAll('a[href*="#"]')
+
+		$(links).on("click", function(e){
+			const anchor = $(this)
+			$('html, body').stop().animate({
+				scrollTop: $(anchor.attr('href')).offset().top
+			}, 777);
+			e.preventDefault()
+			return false;
+		})
+	}
+
+	linkScroll(basket)
+
 //	Dropdown
 	const dropdownToggle = document.querySelector('.dropdown-toggle')
 	const dropdownMenu = document.querySelector('.dropdown-menu')
 
 	dropdownToggle.addEventListener('click', () => {
 		dropdownMenu.classList.toggle('active')
+	})
+
+//	Add to basket
+	const addBtnsBasket = document.querySelectorAll('.product__add')
+	const basketCounter = document.querySelector('.basket__count')
+
+	let count = 0
+	addBtnsBasket.forEach(btn => {
+		btn.addEventListener('click', () => {
+			count++
+
+			basketCounter.innerHTML = `${count}`
+			if (!basketCounter.classList.contains('addProduct')) basketCounter.classList.add('addProduct')
+
+			btn.innerText = '+1'
+			btn.style.backgroundColor = '#fff'
+			btn.style.color = '#000'
+			btn.style.transition = 'all 0.2s ease-in'
+
+			setTimeout(() => {
+				btn.innerText = 'Add to Basket'
+				btn.style.backgroundColor = '#000'
+				btn.style.color = '#fff'
+			}, 800)
+		})
+
 	})
 })
